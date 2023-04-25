f5f2b704adb82d46a80e0cf180ddfcc9579c34cd
John M <github.fox-io@foxdata.io>
2023-04-25 04:54:59 -0500

    Game Over
    Added a game over state and screen which allows player to start a new game.
    
    Fixed computer player logic.

--- main.py
+++ main.py
@@ -72,7 +72,10 @@ class Game:
         8: [3, 5, 7]
     }
 
-    is_player_turn = True
+    is_player_turn = True       # Is true when it is the player's turn.
+    is_active = True            # Is false when game is over.
+    game_over_screen = None     # The game over screen.
+    game_over_button = None     # The game over button.
 
     def __init__(self):
         """Initializes the game.
@@ -82,7 +85,51 @@ class Game:
         pygame.display.set_caption('Tic Tac Toe')
         self.background = pygame.image.load('board.png')
         self.font = pygame.font.Font(None, 64)
+
         self.define_spaces()
+
+    def create_game_over_screen(self, winner):
+        """Creates a game over screen.
+
+        :param winner: The winner of the game.
+        """
+        # Create necessary surfaces.
+        self.game_over_screen = {
+            'background': pygame.Surface((200, 200)),
+            'button_border': pygame.Surface((101, 51)),
+            'button': pygame.Surface((100, 50))
+        }
+
+        # Fill the background with white.
+        self.game_over_screen['background'].fill((255, 255, 255))
+        self.game_over_screen['background'].set_alpha(200)
+
+        # Fill the button border with black.
+        self.game_over_screen['button_border'].fill((0, 0, 0))
+        self.game_over_screen['button_border'].set_alpha(200)
+
+        # Fill the button with white.
+        self.game_over_screen['button'].fill((255, 255, 255))
+        self.game_over_screen['button'].set_alpha(200)
+
+        # Add the text for the winner and the play button.
+        if not winner:
+            self.game_over_screen['background'].blit(self.font.render("Tie!", True, (0, 0, 0)), (60, 20))
+        else:
+            self.game_over_screen['background'].blit(self.font.render(winner + " wins!", True, (0, 0, 0)), (20, 20))
+        self.game_over_screen['button'].blit(self.font.render("Play", True, (0, 0, 0)), (4, 2))
+
+        # Add the button to the button border.
+        self.game_over_screen['button_border'].blit(self.game_over_screen['button'], (0, 0))
+
+        # Add the button border to the background.
+        self.game_over_screen['background'].blit(self.game_over_screen['button_border'], (50, 100))
+
+        # Add the background to the window.
+        self.window.blit(self.game_over_screen['background'], (0, 0))
+
+        # Update the display.
+        pygame.display.flip()
 
     def define_spaces(self):
         """Define the clickable play areas.
@@ -104,7 +151,19 @@ class Game:
 
                 self.window.blit(self.board[space]['surface'], self.board[space]['rect'].move(19, 12))
 
+        if not self.is_active:
+            self.create_game_over_screen(self.check_for_winner())
+
         pygame.display.flip()
+
+    def reset_game(self):
+        """Resets the game.
+        """
+        for space in self.board:
+            self.board[space]['value'] = None
+
+        self.is_active = True
+        self.is_player_turn = True
 
     def on_click(self, pos):
         """Called when player clicks on a space.
@@ -115,6 +174,11 @@ class Game:
         """
         if not self.is_player_turn:
             print("Not your turn.")
+        elif not self.is_active:
+            if self.game_over_screen['button'].get_rect().move(50, 100).collidepoint(pos):
+                self.reset_game()
+            else:
+                print("Game is not active.")
         else:
             for space in self.board:
                 if self.board[space]['rect'].collidepoint(pos):
@@ -127,11 +191,17 @@ class Game:
                         print("Space already taken.")
 
     def make_winning_move(self):
-        """Check for a winning move for th computer player.
+        """Check for a winning move for the computer player.
         """
         for line in self.winning_lines:
             if self.board[self.winning_lines[line][0]]['value'] == 'O' and self.board[self.winning_lines[line][1]]['value'] == 'O' and self.board[self.winning_lines[line][2]]['value'] is None:
                 self.board[self.winning_lines[line][2]]['value'] = 'O'
+                return True
+            if self.board[self.winning_lines[line][0]]['value'] == 'O' and self.board[self.winning_lines[line][1]]['value'] is None and self.board[self.winning_lines[line][2]]['value'] == 'O':
+                self.board[self.winning_lines[line][1]]['value'] = 'O'
+                return True
+            if self.board[self.winning_lines[line][0]]['value'] is None and self.board[self.winning_lines[line][1]]['value'] == 'O' and self.board[self.winning_lines[line][2]]['value'] == 'O':
+                self.board[self.winning_lines[line][0]]['value'] = 'O'
                 return True
         return False
 
@@ -139,8 +209,14 @@ class Game:
         """Check for a blocking move for the computer player.
         """
         for line in self.winning_lines:
-            if self.board[self.winning_lines[line][0]]['value'] == 'O' and self.board[self.winning_lines[line][1]]['value'] == 'O' and self.board[self.winning_lines[line][2]]['value'] is None:
+            if self.board[self.winning_lines[line][0]]['value'] == 'X' and self.board[self.winning_lines[line][1]]['value'] == 'X' and self.board[self.winning_lines[line][2]]['value'] is None:
                 self.board[self.winning_lines[line][2]]['value'] = 'O'
+                return True
+            elif self.board[self.winning_lines[line][0]]['value'] == 'X' and self.board[self.winning_lines[line][1]]['value'] is None and self.board[self.winning_lines[line][2]]['value'] == 'X':
+                self.board[self.winning_lines[line][1]]['value'] = 'O'
+                return True
+            elif self.board[self.winning_lines[line][0]]['value'] is None and self.board[self.winning_lines[line][1]]['value'] == 'X' and self.board[self.winning_lines[line][2]]['value'] == 'X':
+                self.board[self.winning_lines[line][0]]['value'] = 'O'
                 return True
 
     def make_center_move(self):
@@ -163,11 +239,16 @@ class Game:
     def make_computer_turn(self):
         """Perform logic needed for the computer player to make a turn.
         """
+        if not self.is_active:
+            return
+
         if not self.make_winning_move():
             if not self.make_blocking_move():
                 if not self.make_center_move():
                     if not self.make_random_move():
-                        print("No moves left.")
+                        # Tie game
+                        self.is_active = False
+
         if self.check_for_winner() == 'O':
             print("Computer wins!")
 
@@ -176,7 +257,12 @@ class Game:
         """
         for line in self.winning_lines:
             if self.board[self.winning_lines[line][0]]['value'] == self.board[self.winning_lines[line][1]]['value'] == self.board[self.winning_lines[line][2]]['value']:
-                return self.board[self.winning_lines[line][0]]['value']
+                if self.board[self.winning_lines[line][0]]['value'] == 'X':
+                    self.is_active = False
+                    return 'X'
+                elif self.board[self.winning_lines[line][0]]['value'] == 'O':
+                    self.is_active = False
+                    return 'O'
         return None
 
 
